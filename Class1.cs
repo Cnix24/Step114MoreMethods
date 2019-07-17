3965c99197e8302d6146aace585fc19e5d41085f
Cnix24 <calvinattl24@gmail.com>
2019-07-17 15:25:23 -0600

    Update

---
+++ Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step114MoreMethods
+{
+    public class MethodClass
+    {
+        public int Operation(int x)
+        {
+            int answer = x + 33;
+            return answer;
+        }
+        public int Operation(double x)
+        {
+            double integerAnswer = x + 12;
+            int answer = Convert.ToInt32(integerAnswer);
+            return answer;
+        }
+        public int Operation(string x)
+        {
+            int integerX = Convert.ToInt32(x);
+            int answer = integerX + 95;
+
+            return answer;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step114MoreMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MethodClass integer = new MethodClass();
+            int answer = integer.Operation(x: 12);
+
+            Console.WriteLine(answer);
+
+            MethodClass integerTwo = new MethodClass();
+            int answerTwo = integerTwo.Operation(x: 99.6);
+            
+            Console.WriteLine(answerTwo);
+
+            MethodClass integerThree = new MethodClass();
+            int answerThree = integerThree.Operation("100");
+
+            Console.WriteLine(answerThree);
+        }
+    }
+}
